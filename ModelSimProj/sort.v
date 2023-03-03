7ebb1588a7a09eeba30835d7ae01065d7dc58cea
XuanDuc0308 <93237732+XuanDuc0308@users.noreply.github.com>
2023-03-03 21:59:36 +0700

    Add files via upload

---
+++ ModelSimProj/sort.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+module sort #( parameter NUM_VALS = 9, parameter SIZE = 8) 
+    (
+    input  wire clk,
+    input  wire [NUM_VALS*SIZE-1:0] in,
+    output reg  [NUM_VALS*SIZE-1:0] out,
+    output reg   [8-1:0] median
+    );
+    reg [NUM_VALS*SIZE-1:0] sorted_bus;
+    always @(posedge clk) begin
+        out <= sorted_bus;
+        median <= sorted_bus[39:32];
+    end
+    integer i, j;
+    reg [SIZE-1:0] temp;
+    reg [SIZE-1:0] array [1:NUM_VALS];
+    always @* begin
+        for (i = 0; i < NUM_VALS; i = i + 1) begin
+            array[i+1] = in[i*SIZE +: SIZE];
+        end
+        for (i = NUM_VALS; i > 0; i = i - 1) begin
+            for (j = 1 ; j < i; j = j + 1) begin
+                if (array[j] < array[j + 1]) begin
+                    temp         = array[j];
+                    array[j]     = array[j + 1];
+                    array[j + 1] = temp;
+                end 
+            end
+        end
+        for (i = 0; i < NUM_VALS; i = i + 1) begin
+            sorted_bus[i*SIZE +: SIZE] = array[i+1];
+        end
+    end
+endmodule
+
---
+++ ModelSimProj/testbench.v
@@ -0,0 +1,40 @@
+`timescale 1ns/1ps
+module sort_tb;
+    reg clk;
+    reg  [8-1:0] in1,  in2,  in3,  in4,  in5 , in6 , in7, in8, in9;
+    wire [8-1:0] out1, out2, out3, out4, out5 ,out6,out7,out8,out9,median;
+
+    sort #(.NUM_VALS(9), .SIZE(8)) dut (
+        .clk(clk),
+        .in ({in1,  in2,  in3,  in4,  in5 , in6 , in7, in8, in9}),
+        .out({out1, out2, out3, out4, out5,out6,out7,out8,out9}),
+        .median({median})
+    );
+    always @(posedge clk) begin
+        in1 <= $random;
+        in2 <= $random;
+        in3 <= $random;
+        in4 <= $random;
+        in5 <= $random;
+        in6 <= $random;
+        in7 <= $random;
+        in8 <= $random;
+        in9 <= $random;
+    end
+    always @(posedge clk) begin
+        $display("In:  %0d %0d %0d %0d %0d %0d %0d %0d %0d", in1,  in2,  in3,  in4,  in5, in6 , in7, in8, in9);
+        $display("Out: %0d %0d %0d %0d %0d %0d %0d %0d %0d", out1, out2, out3, out4, out5, out6, out7, out8, out9);
+        $display("Median: %0d", median);
+    end
+    initial begin
+        #100;
+        $finish;
+    end
+
+    always begin
+        clk = 1'b0; #5;
+        clk = 1'b1; #5;
+    end
+endmodule
+
+
